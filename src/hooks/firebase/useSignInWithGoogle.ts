8a2abaca23c637a38d7c44d6f1d76f8c9bf817c8
unknown <srki.sanadrovic@gmail.com>
2022-10-24 15:27:18 +0200

    [REFACTOR] auth with popup

--- src/hooks/firebase/useSignInWithGoogle.ts
+++ src/hooks/firebase/useSignInWithGoogle.ts
@@ -1,37 +1,15 @@
-import {
-	getRedirectResult,
-	GoogleAuthProvider,
-	signInWithPopup,
-	signInWithRedirect,
-} from "firebase/auth"
-import { Provider } from "react-redux"
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 import { auth, authProvider } from "../../firebase/app"
 import { createUserInDb } from "../../firebase/createUserInDb"
 
 export async function useSignInWithGoogle() {
 	try {
-		// const result = await signInWithPopup(auth, authProvider)
+		const result = await signInWithPopup(auth, authProvider)
 
-		// const credential = GoogleAuthProvider.credentialFromResult(result)
-		// const user = result.user
+		const credential = GoogleAuthProvider.credentialFromResult(result)
+		const user = result.user
 
-		// createUserInDb(user.uid, user.displayName || "Annonymous")
-
-		await signInWithRedirect(auth, authProvider)
-
-		try {
-			const result = await getRedirectResult(auth)
-
-			const user = result?.user
-
-			if (user) {
-				createUserInDb(user.uid, user.displayName || "Annonymous")
-			} else {
-				console.log("foo")
-			}
-		} catch (error) {
-			console.error(error)
-		}
+		createUserInDb(user.uid, user.displayName || "Annonymous")
 	} catch (error) {
 		console.error("Error in authentication", error)
 	}
